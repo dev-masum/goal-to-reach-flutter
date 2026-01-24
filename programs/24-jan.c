9fa95baa4e848485bd3956a7f66a84886576c6e6
Abdullah al Masum <abdullahalmasum.dev@gamil.com>
2026-01-24 11:38:50 +0600

    added 24 jan docs

---
+++ programs/24-jan.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int add()
+{
+    printf("add");
+    return 0;
+}
+int sub()
+{
+    printf("sub");
+    return 0;
+}
+
+int main()
+{
+    printf("hello");
+    add();
+    sub();
+    return 0;
+}
